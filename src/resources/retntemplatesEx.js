357046ad3d6c5b967706825f18312275d151c5e6
Szabó Roland <szrtanul@gmail.com>
2025-09-09 08:09:48 +0200

    syncmaster

--- src/resources/retntemplatesEx.js
+++ src/resources/retntemplatesEx.js
@@ -50,7 +50,7 @@ export const retnCombinations = {
     pg_columns: retEx[0]("308"),
     pg_proc: retEx[0]("0'+GQTproc'"),
     pg_procpro: retEx[0]("0'+GQTprocpro'"),
-    profile: "getDataLength:theadevcslist:tablerow:tbodyend|||00FFFFFF;",
+    userperson: "getDataLength:theadevcslist:tablerow:tbodyend:personev|||00FFFFFF;30A---020103FF;309---FF04FFFF;30A;:0,1",
     profilev: retEx[0]("309"),
     personev: retEx[0]("30A")
 }
